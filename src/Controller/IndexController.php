b73ec0c13b2a5b9aed11289e5db2dfe6d2f2a3cf
Niels Zwart <nielszwart@hotmail.com>
2018-10-05 14:32:08 +0200

    x

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,11 +4,22 @@ namespace App\Controller;
 
 use App\Controller\BaseController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Event;
 
 class IndexController extends BaseController
 {
     public function homepage()
     {
-        return $this->render('website/homepage.twig', ['whatever' => 'hoi']);
+    	$event = $this->getDoctrine()->getRepository(Event::class)->findOneBy(['slug' => 'mario-kart']);
+
+   		echo "<pre>";
+   		var_dump($event->getName());
+   		exit;
+
+        return $this->render(
+        	'website/homepage.twig', [
+        		'whatever' => 'hoi'
+    	    ]
+    	);
     }
 }
